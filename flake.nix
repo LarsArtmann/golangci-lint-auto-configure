99a678f46c14d6c3d54fb1153b7e20aa50854e4f
Lars Artmann <git@lars.software>
2026-05-25 06:47:56 +0200

    wip: reset vendorHash

--- flake.nix
+++ flake.nix
@@ -66,7 +66,7 @@
 
           proxyVendor = true;
 
-          vendorHash = "sha256-VeOlYERMe0vbf1eBDgNGsQun5mlleTsCiu/ncLTO9AA=";
+          vendorHash = "";
 
           subPackages = [ "cmd/golangci-lint-auto-configure" ];
 
