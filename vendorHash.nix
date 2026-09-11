d00f1ca20851cccac9d665b3eefa78b03819d421
Lars Artmann <git@lars.software>
2026-09-11 07:21:05 +0200

    chore: auto-commit 1 changed file(s) (heuristic)

--- vendorHash.nix
+++ vendorHash.nix
@@ -1,2 +1,2 @@
 # Managed by buildflow nix-checker — update with: nix build 2>&1 | rg "got:"
-"sha256-FeWRnA7fnvDhQjXS3DJHbk2KckFDTQMq2Gd9qPABNVU="
+"sha256-egL4sgSye9/rQdo4DtGuJI8PabFOlqMC2lmhcFPAf1c="
