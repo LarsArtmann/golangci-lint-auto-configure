6d2216791719d994ae4e4352003fcbb91ab83de8
Lars Artmann <git@lars.software>
2026-09-11 11:50:34 +0200

    chore: auto-commit 1 changed file(s) (heuristic)

--- pkg/linter/fixer_test.go
+++ pkg/linter/fixer_test.go
@@ -415,7 +415,8 @@ linters:
 				)
 			},
 			Entry("wsl -> wsl_v5", "wsl", "wsl_v5", "wsl:"),
-			Entry("gomodguard -> gomodguard_v2", "gomodguard", "gomodguard_v2", "- gomodguard\n"),
+			Entry("gomodguard -> gomodguard_v2", "gomodguard", "gomodguard_v2", "- gomodguard\n"\n"),
+			Entry("exhaustruct -> exhaustruct_v5", "exhaustruct", "exhaustruct_v5", "- exhaustruct\n"),
 		)
 
 		It("should detect deprecated linter gomodguard and suggest gomodguard_v2", func() {
